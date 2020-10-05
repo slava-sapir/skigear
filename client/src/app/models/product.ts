4d061618ee40c591a3f75e9326b5c931c3dd7f33
slava sapir <slavas21@gmail.com>
2020-10-05 17:37:24 -0400

    End of section 8

---
+++ client/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    pictureUrl: string;
+    productType: string;
+    productBrand: string;
+  }
