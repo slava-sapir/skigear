43bd87d72565278e441be2565804a95414318661
slava sapir <slavas21@gmail.com>
2020-10-15 18:14:56 -0400

    End of section 13

--- API/Extentions/ApplicationServicesExtentions.cs
+++ API/Extentions/ApplicationServicesExtentions.cs
@@ -12,7 +12,9 @@ namespace API.Extentions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            
             services.Configure<ApiBehaviorOptions>(options =>
             {
                 options.InvalidModelStateResponseFactory = actionContext =>
