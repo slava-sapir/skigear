c40febbc689dd5dc1fa7626519cae0695ee156bc
slava sapir <slavas21@gmail.com>
2020-10-22 15:02:08 -0400

    End section16

--- API/Dto/RegisterDto.cs
+++ API/Dto/RegisterDto.cs
@@ -1,9 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dto
 {
     public class RegisterDto
     {
+        private const string V = "Password must have 1 uppercase, 1 lowcase, 1 alphanumeric, 1 number and at least 6 charachters";
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage = V)]
         public string Password { get; set; }
         
     }
--- API/Extentions/SwaggerServiceExtentions.cs
+++ API/Extentions/SwaggerServiceExtentions.cs
@@ -11,6 +11,23 @@ namespace API.Extentions
              services.AddSwaggerGen(c => 
             {
                 c.SwaggerDoc("v1", new OpenApiInfo{ Title = "SkiGear API", Version = "v1"});
+                var securityScheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Sheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securityScheme);
+                var securityRequirements = new OpenApiSecurityRequirement{{ securityScheme, new[]
+                {"Bearer"}}};
+                c.AddSecurityRequirement(securityRequirements);
             });
 
             return services;
